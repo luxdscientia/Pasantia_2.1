7a1734df0833a7c598609b973f5dc94b4f5b2b75
Jose Guevara <94gjose@gmail.com>
2024-08-03 10:54:26 -0400

    guardar datos de las pruebas para posterior envio

--- lucia-tesis-v2/lucia-tesis-v2.ino
+++ lucia-tesis-v2/lucia-tesis-v2.ino
@@ -9,9 +9,16 @@ const int INICIO = 12;
 const int RELES_SRV[2] = { 2, 3 };
 const int RELES_SW[2] = { 4, 5 };
 
+const char nombre_pruebas[4] = {'A', 'B', 'C', 'D'};
+
+int numero_prueba = 0;
+float pruebas[4][8];
+
 void setup() {
   // Configurar pines de entrada y salida
   Serial.begin(9600);
+  Serial1.begin(9600);
+
   lcd.begin(16, 2);
   pinMode(INICIO, INPUT);
   //RELES
@@ -32,6 +39,7 @@ void mostrarPantalla(String linea1, String linea2) {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(linea1);
+
   lcd.setCursor(0, 1);
   lcd.print(linea2);
 }
@@ -39,9 +47,24 @@ void mostrarPantalla(String linea1, String linea2) {
 void loop() {
   mostrarPantalla("encendido","....");
   if (digitalRead(INICIO) == HIGH) {
+    numero_prueba = 0;
     for (auto& configuracion : CONF_PRUEBAS_RV) {
       realizarPrueba(configuracion);
+      numero_prueba ++;
       delay(1000);
+    }
+
+    for (int i = 0; i < 4; i ++) {
+      for (int j = 0; j < 6; j++) {
+        Serial1.print(pruebas[i][j]);
+        Serial1.print(", ");
+      }
+      Serial1.println();
     }
   }
 }
+
+void Debug(String mensaje) {
+  Serial1.println(mensaje);
+}
+
--- lucia-tesis-v2/prueba.ino
+++ lucia-tesis-v2/prueba.ino
@@ -3,61 +3,52 @@ void setConfiguracionPrueba(int configuracion[2]) {
   digitalWrite(RELES_SRV[0], configuracion[0]);
   digitalWrite(RELES_SRV[1], configuracion[1]);
 }
+
 void encender_reles(int reles[2], int valores[2]) {
   for (int i = 0; i < 2; i++) {
     digitalWrite(reles[i], valores[i]);
   }
 }
-float v1[9];
-float v2[9];
+
 
 void configurarPrueba(int red[2], int i) {
   // Prender Reles
   encender_reles(RELES_SW, CONF_PROCESOS[i]);
 }
 
-
-void hacerPrueba(int i) {
+int prueba_interna;
+void hacerPrueba() {
+  float v1;
+  float v2;
   // Mientras 2 ciclos de red
   // Proceso A
   //v1
   configurar_medicion_alterna(566, 10);  //referencia de corriente
   while (loop_medicion_alterna(&get_digital_V1))
     ;
-  v1[i] = obtener_valor_rms();
-  Serial.print("V1: ");
-  Serial.println(v1[i]);
+  v1 = obtener_valor_rms();
+  pruebas[numero_prueba][prueba_interna] = v1;
+  prueba_interna++;
   // v2
   configurar_medicion_alterna(707, 10);  //referencia de voltaje
   while (loop_medicion_alterna(&get_digital_V2))
     ;
-  v2[i] = obtener_valor_rms();
-  Serial.print("V2: ");
-  Serial.println(v2[i]);
+  v2 = obtener_valor_rms();
+  pruebas[numero_prueba][prueba_interna] = v1;
+  prueba_interna++;
+  Debug("Test: " + String(prueba_interna) + ", V1:" + String(v1) + ", V2: " + String(v2));
 }
-
-void enviarDatosPrueba() {
-  // Mostrar los datos serial
-  for (int i = 0; i < 10; i++) {
-    Serial.print("Proceso ");
-    Serial.println(i);
-    Serial.print("V1: ");
-    Serial.println(v1[i]);
-    Serial.print("V2: ");
-    Serial.println(v2[i]);
-  }
-}
-
 
 void realizarPrueba(int configuracion[2]) {
-  Serial.println("inicio config");
+  prueba_interna = 0;
+  Debug("Inicio configarion " + String(nombre_pruebas[numero_prueba]));
+
   setConfiguracionPrueba(configuracion);
 
   for (int i = 0; i < 3; i++) {
     configurarPrueba(RELES_SW, i);
-    hacerPrueba(i);
+    hacerPrueba();
   }
 
-  enviarDatosPrueba();
   delay(1000);
 }
