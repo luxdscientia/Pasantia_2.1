e5e6cd6a2e373fee5d44165acf35ffbb1d588cbe
Jose Guevara <94gjose@gmail.com>
2024-08-13 11:45:33 -0400

    clear trash in the code

--- Grafica_M/Grafica_M/Grafica_M.ino
+++ Grafica_M/Grafica_M/Grafica_M.ino
@@ -1,11 +1,10 @@
-#include "TrueRMS.h"
 
 #define RMS_WINDOW 50  // 50 muestras
 #define PERIODO 1000   // 1 ms Periodo muestra
 const int INICIO = 12;
 
 unsigned long nextLoop;
-Rms readRms;
+
 int count;
 int samples[RMS_WINDOW];
 bool cruzoPorCero = false;
@@ -13,8 +12,6 @@ int crucesPorCero = 0;
 bool cruces[RMS_WINDOW];  // Vector para marcar los cruces por cero
 
 void configurar_medicion_alterna(float rango_voltaje, unsigned char bits) {
-  // readRms.begin(rango_voltaje, RMS_WINDOW, bits, BLR_ON, CNT_SCAN);
-  // readRms.start();
   count = 0;
   nextLoop = micros() + PERIODO;
   cruzoPorCero = false;
@@ -22,19 +19,9 @@ void configurar_medicion_alterna(float rango_voltaje, unsigned char bits) {
   memset(cruces, 0, sizeof(cruces));  // Inicializar el vector de cruces por cero
 }
 
-void configurar_medicion_alterna_V1(float rango_voltaje, unsigned char bits) {
-  readRms.begin(rango_voltaje, RMS_WINDOW, bits, BLR_ON, CNT_SCAN);
-
-  readRms.start();
-
-  count = 0;
-  nextLoop = micros() + PERIODO;
-}
-
 bool detectar_cruce_por_cero() {
   static int valorAnterior = 0;
   int valorActual = analogRead(A1);
-  // delayMicroseconds(200);
   bool ocurrio_cruce = (valorAnterior < 512 && valorActual >= 512) || (valorAnterior > 512 && valorActual <= 512);
   valorAnterior = valorActual;
   return ocurrio_cruce;
@@ -49,8 +36,7 @@ bool loop_medicion_alterna() {
   }
 
   int medicion = analogRead(A1);
-  // delayMicroseconds(300);
-  // readRms.update(medicion);
+
   samples[count] = medicion;
   cruces[count] = detectar_cruce_por_cero();  // Marcar si hay cruce por cero
 
@@ -71,26 +57,6 @@ void enviando_valores() {
   }
 }
 
-bool loop_medicion_alterna_V1(int (*obtener_valor)()) {
-  int medicion = obtener_valor();
-  readRms.update(medicion);
-
-  while (nextLoop > micros())
-    ;
-  nextLoop += micros() + PERIODO;
-  count++;
-  return count >= 1000 ? false : true;
-}
-
-float obtener_valor_rms() {
-
-  readRms.publish();
-  return readRms.rmsVal;
-}
-
-int get_digital_V1() {
-  return analogRead(A1);
-}
 
 void setup() {
   Serial.begin(9600);
@@ -102,14 +68,6 @@ void loop() {
   if (digitalRead(INICIO) == HIGH) {
     if (!loop_medicion_alterna()) {
       enviando_valores();
-      // float V1;
-      // configurar_medicion_alterna_V1(700, 10);
-      // while (loop_medicion_alterna_V1(&get_digital_V1))
-      //   ;
-      // V1 = obtener_valor_rms();
-      // Serial.println("V2: " + String(V1));
-      // delay(5000);
-
 
       configurar_medicion_alterna(5.0, 10);  // Configurar nuevamente
     }
